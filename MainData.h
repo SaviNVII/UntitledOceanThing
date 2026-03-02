c397e0f6138c57028b4f692d777994ca994adc7f
SaviN7 <savinseven7@gmail.com>
2026-03-02 13:03:46 -0500

    a

---
+++ MainData.h
@@ -0,0 +1,11 @@
+//
+// Created by om0002 on 3/2/2026.
+//
+
+#ifndef UNTITLEDOCEANTHING_MAINDATA_H
+#define UNTITLEDOCEANTHING_MAINDATA_H
+
+extern float posX;
+extern float posY;
+
+#endif //UNTITLEDOCEANTHING_MAINDATA_H
